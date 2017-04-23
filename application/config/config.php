cf3eb350be89801ce8642e9115a46aa305f127bf
z0rgan <mkdbht@gmail.com>
2017-04-24 00:10:07 +0545

    dd

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://139.59.60.17/books/index.php/';
+$config['base_url'] = 'http://139.59.60.17/books/';
 
 /*
 |--------------------------------------------------------------------------
