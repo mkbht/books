577090402c0c88311fadb219799c65674f080c19
z0rgan <mkdbht@gmail.com>
2017-04-24 00:20:17 +0545

    dd

---
+++ application/config/pagination.php
@@ -0,0 +1,19 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+// application/config/bootstrap_pagination.php
+$config['pagination']['full_tag_open'] = '<ul class="pagination">';
+$config['pagination']['full_tag_close'] = '</ul>';
+$config['pagination']['first_tag_open'] = '<li>';
+$config['pagination']['first_tag_close'] = '</li>';
+$config['pagination']['last_tag_open'] = '<li>';
+$config['pagination']['last_tag_close'] = '</li>';
+$config['pagination']['next_link'] = '&raquo;';
+$config['pagination']['next_tag_open'] = '<li title="Next">';
+$config['pagination']['next_tag_close'] = '</li>';
+$config['pagination']['prev_link'] = '&laquo;';
+$config['pagination']['prev_tag_open'] = '<li title="Previous">';
+$config['pagination']['prev_tag_close'] = '</li>';
+$config['pagination']['cur_tag_open'] = '<li class="active"><span>';
+$config['pagination']['cur_tag_close'] = '</span></li>';
+$config['pagination']['num_tag_open'] = '<li>';
+$config['pagination']['num_tag_close'] = '</li>';
+?>
